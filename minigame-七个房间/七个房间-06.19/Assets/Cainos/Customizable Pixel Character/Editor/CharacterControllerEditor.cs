c5d24be70970edf5e4add6bbc8a03eea2d599c32
ying-star <56780703+ying-star@users.noreply.github.com>
2021-12-14 11:17:48 +0800

    作品源代码和一些资料

---
+++ minigame-七个房间/七个房间-06.19/Assets/Cainos/Customizable Pixel Character/Editor/CharacterControllerEditor.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Cainos
+{
+
+    [CustomEditor(typeof(CharacterController))]
+    public class CharacterControllerEditor : Editor
+    {
+        private CharacterController instance;
+
+        private SerializedProperty defaultMovement;
+        private SerializedProperty leftKey;
+        private SerializedProperty rightKey;
+        private SerializedProperty crouchKey;
+        private SerializedProperty jumpKey;
+        private SerializedProperty moveModifierKey;
+        private SerializedProperty attackKey;
+
+        private SerializedProperty walkSpeedMax;
+        private SerializedProperty walkAcc;
+        private SerializedProperty runSpeedMax;
+        private SerializedProperty runAcc;
+        private SerializedProperty crouchSpeedMax;
+        private SerializedProperty crouchAcc;
+        private SerializedProperty airSpeedMax;
+        private SerializedProperty airAcc;
+        private SerializedProperty groundBrakeAcc;
+        private SerializedProperty airBrakeAcc;
+        private SerializedProperty jumpSpeed;
+        private SerializedProperty jumpCooldown;
+        private SerializedProperty jumpGravityMutiplier;
+        private SerializedProperty fallGravityMutiplier;
+
+        private SerializedProperty groundCheckRadius;
+
+        private PropertyField IsDead;
+
+
+        private bool foldout_input = true;
+        private bool foldout_movement = true;
+        private bool foldout_runtime = true;
+
+        private void OnEnable()
+        {
+            instance = target as CharacterController;
+
+            defaultMovement = serializedObject.FindProperty("defaultMovement");
+            leftKey = serializedObject.FindProperty("leftKey");
+            rightKey = serializedObject.FindProperty("rightKey");
+            crouchKey = serializedObject.FindProperty("crouchKey");
+            jumpKey = serializedObject.FindProperty("jumpKey");
+            moveModifierKey = serializedObject.FindProperty("moveModifierKey");
+            attackKey = serializedObject.FindProperty("attackKey");
+
+            walkSpeedMax = serializedObject.FindProperty("walkSpeedMax");
+            walkAcc = serializedObject.FindProperty("walkAcc");
+            runSpeedMax = serializedObject.FindProperty("runSpeedMax");
+            runAcc = serializedObject.FindProperty("runAcc");
+            crouchSpeedMax = serializedObject.FindProperty("crouchSpeedMax");
+            crouchAcc = serializedObject.FindProperty("crouchAcc");
+            airSpeedMax = serializedObject.FindProperty("airSpeedMax");
+            airAcc = serializedObject.FindProperty("airAcc");
+            airSpeedMax = serializedObject.FindProperty("airSpeedMax");
+            groundBrakeAcc = serializedObject.FindProperty("groundBrakeAcc");
+            groundBrakeAcc = serializedObject.FindProperty("groundBrakeAcc");
+            airBrakeAcc = serializedObject.FindProperty("airBrakeAcc");
+            jumpSpeed = serializedObject.FindProperty("jumpSpeed");
+            jumpCooldown = serializedObject.FindProperty("jumpCooldown");
+            jumpGravityMutiplier = serializedObject.FindProperty("jumpGravityMutiplier");
+            fallGravityMutiplier = serializedObject.FindProperty("fallGravityMutiplier");
+
+            groundCheckRadius = serializedObject.FindProperty("groundCheckRadius");
+
+            IsDead = ExposeProperties.GetProperty("IsDead", instance);
+
+        }
+
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+
+            foldout_input = EditorGUILayout.Foldout(foldout_input, "Input", true);
+            if (foldout_input)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(defaultMovement);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(leftKey);
+                EditorGUILayout.PropertyField(rightKey);
+                EditorGUILayout.PropertyField(crouchKey);
+                EditorGUILayout.PropertyField(jumpKey);
+                EditorGUILayout.PropertyField(moveModifierKey);
+                EditorGUILayout.PropertyField(attackKey);
+                EditorGUI.indentLevel--;
+            }
+
+
+            foldout_movement = EditorGUILayout.Foldout(foldout_movement, "Movement", true);
+            if (foldout_movement)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(walkSpeedMax);
+                EditorGUILayout.PropertyField(walkAcc);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(runSpeedMax);
+                EditorGUILayout.PropertyField(runAcc);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(crouchSpeedMax);
+                EditorGUILayout.PropertyField(crouchAcc);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(airSpeedMax);
+                EditorGUILayout.PropertyField(airAcc);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(groundBrakeAcc);
+                EditorGUILayout.PropertyField(airBrakeAcc);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(jumpSpeed);
+                EditorGUILayout.PropertyField(jumpCooldown);
+                EditorGUILayout.PropertyField(jumpGravityMutiplier);
+                EditorGUILayout.PropertyField(fallGravityMutiplier);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(groundCheckRadius);
+                EditorGUI.indentLevel--;
+            }
+
+            foldout_runtime = EditorGUILayout.Foldout(foldout_runtime, "Runtime", true);
+            if (foldout_runtime)
+            {
+                EditorGUI.indentLevel++;
+                ExposeProperties.Expose(IsDead);
+                EditorGUI.indentLevel--;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ minigame-七个房间/七个房间-06.19/Assets/Cainos/Customizable Pixel Character/Editor/PixelCharacterEditor.cs
@@ -0,0 +1,190 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+using Cainos;
+
+[CustomEditor(typeof(PixelCharacter))]
+public class PixelCharacterEditor : Editor
+{
+    private PixelCharacter instance;
+
+    private SerializedProperty animator;
+    private SerializedProperty hat;
+    private SerializedProperty hair;
+    private SerializedProperty hairClipped;
+    private SerializedProperty eye;
+    private SerializedProperty eyeBase;
+    private SerializedProperty facewear;
+    private SerializedProperty cloth;
+    private SerializedProperty skirt;
+    private SerializedProperty pants;
+    private SerializedProperty socks;
+    private SerializedProperty shoes;
+    private SerializedProperty back;
+    private SerializedProperty expression;
+    private SerializedProperty body;
+    private SerializedProperty weaponSlot;
+
+    private PropertyField HatMaterial;
+    private PropertyField HairMaterial;
+    private PropertyField EyeMaterial;
+    private PropertyField EyeBaseMaterial;
+    private PropertyField FacewearMaterial;
+    private PropertyField ClothMaterial;
+    private PropertyField PantsMaterial;
+    private PropertyField SocksMaterial;
+    private PropertyField ShoesMaterial;
+    private PropertyField BackMaterial;
+    private PropertyField BodyMaterial;
+    private PropertyField ClipHair;
+    private SerializedProperty blinkInterval;
+
+    private PropertyField Facing;
+    private PropertyField Expression;
+    private PropertyField AttackAction;
+    private PropertyField IsCrouching;
+    private PropertyField IsGrounded;
+    private PropertyField IsAttacking;
+    private PropertyField IsDead;
+    private PropertyField MovingBlend;
+
+    private bool foldout_objects = false;
+    private bool foldout_appearance = true;
+    private bool foldout_params = true;
+    private bool foldout_runtime = true;
+
+    public void OnEnable()
+    {
+        instance = target as PixelCharacter;
+
+        animator = serializedObject.FindProperty("animator");
+        hat = serializedObject.FindProperty("hat");
+        hair = serializedObject.FindProperty("hair");
+        hairClipped = serializedObject.FindProperty("hairClipped");
+        eye = serializedObject.FindProperty("eye");
+        eyeBase = serializedObject.FindProperty("eyeBase");
+        facewear = serializedObject.FindProperty("facewear");
+        cloth = serializedObject.FindProperty("cloth");
+        skirt = serializedObject.FindProperty("skirt");
+        pants = serializedObject.FindProperty("pants");
+        socks = serializedObject.FindProperty("socks");
+        shoes = serializedObject.FindProperty("shoes");
+        back = serializedObject.FindProperty("back");
+        expression = serializedObject.FindProperty("expression");
+        body = serializedObject.FindProperty("body");
+        weaponSlot = serializedObject.FindProperty("weaponSlot");
+
+        HatMaterial = ExposeProperties.GetProperty("HatMaterial", instance);
+        HairMaterial = ExposeProperties.GetProperty("HairMaterial", instance);
+        EyeMaterial = ExposeProperties.GetProperty("EyeMaterial", instance);
+        EyeBaseMaterial = ExposeProperties.GetProperty("EyeBaseMaterial", instance);
+        FacewearMaterial = ExposeProperties.GetProperty("FacewearMaterial", instance);
+        ClothMaterial = ExposeProperties.GetProperty("ClothMaterial", instance);
+        PantsMaterial = ExposeProperties.GetProperty("PantsMaterial", instance);
+        SocksMaterial = ExposeProperties.GetProperty("SocksMaterial", instance);
+        ShoesMaterial = ExposeProperties.GetProperty("ShoesMaterial", instance);
+        BackMaterial = ExposeProperties.GetProperty("BackMaterial", instance);
+        BodyMaterial = ExposeProperties.GetProperty("BodyMaterial", instance);
+        ClipHair = ExposeProperties.GetProperty("ClipHair", instance);
+        blinkInterval = serializedObject.FindProperty("blinkInterval");
+
+        Expression = ExposeProperties.GetProperty("Expression", instance);
+        AttackAction = ExposeProperties.GetProperty("AttackAction", instance);
+        Facing = ExposeProperties.GetProperty("Facing", instance);
+        IsCrouching = ExposeProperties.GetProperty("IsCrouching", instance);
+        IsGrounded = ExposeProperties.GetProperty("IsGrounded", instance);
+        IsAttacking = ExposeProperties.GetProperty("IsAttacking", instance);
+        IsDead = ExposeProperties.GetProperty("IsDead", instance);
+        MovingBlend = ExposeProperties.GetProperty("MovingBlend", instance);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (instance == null) return;
+        serializedObject.Update();
+
+        foldout_objects = EditorGUILayout.Foldout(foldout_objects, "Objects", true);
+        if (foldout_objects)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(animator);
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(hat);
+            EditorGUILayout.PropertyField(hair);
+            EditorGUILayout.PropertyField(hairClipped);
+            EditorGUILayout.PropertyField(eye);
+            EditorGUILayout.PropertyField(eyeBase);
+            EditorGUILayout.PropertyField(facewear);
+            EditorGUILayout.PropertyField(cloth);
+            EditorGUILayout.PropertyField(skirt);
+            EditorGUILayout.PropertyField(pants);
+            EditorGUILayout.PropertyField(socks);
+            EditorGUILayout.PropertyField(shoes);
+            EditorGUILayout.PropertyField(back);
+            EditorGUILayout.PropertyField(expression);
+            EditorGUILayout.PropertyField(body);
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(weaponSlot);
+            EditorGUI.indentLevel--;
+        }
+
+
+        foldout_appearance = EditorGUILayout.Foldout(foldout_appearance, "Appearance", true);
+        if (foldout_appearance)
+        {
+            EditorGUI.indentLevel++;
+            ExposeProperties.Expose(HatMaterial, typeof(Material));
+            ExposeProperties.Expose(HairMaterial, typeof(Material));
+            ExposeProperties.Expose(EyeMaterial , typeof(Material));
+            ExposeProperties.Expose(EyeBaseMaterial, typeof(Material));
+            ExposeProperties.Expose(FacewearMaterial, typeof(Material));
+            ExposeProperties.Expose(ClothMaterial, typeof(Material));
+            ExposeProperties.Expose(PantsMaterial, typeof(Material));
+            ExposeProperties.Expose(SocksMaterial, typeof(Material));
+            ExposeProperties.Expose(ShoesMaterial, typeof(Material));
+            ExposeProperties.Expose(BackMaterial, typeof(Material));
+            ExposeProperties.Expose(BodyMaterial, typeof(Material));
+            EditorGUILayout.Space();
+            ExposeProperties.Expose(ClipHair);
+            EditorGUILayout.PropertyField(blinkInterval);
+
+            EditorGUI.indentLevel--;
+        }
+
+        foldout_runtime = EditorGUILayout.Foldout(foldout_runtime, "Runtime", true);
+        if (foldout_runtime)
+        {
+            EditorGUI.indentLevel++;
+            ExposeProperties.Expose(Expression);
+            ExposeProperties.Expose(AttackAction);
+            ExposeProperties.Expose(Facing);
+            ExposeProperties.Expose(IsCrouching);
+            ExposeProperties.Expose(IsGrounded);
+            ExposeProperties.Expose(IsAttacking);
+            ExposeProperties.Expose(IsDead);
+            ExposeProperties.Expose(MovingBlend);
+            EditorGUILayout.Space();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(EditorGUI.indentLevel * 10);
+            if (GUILayout.Button("Injured Front")) instance.InjuredFront();
+            if (GUILayout.Button("Injured Back")) instance.InjuredBack();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(EditorGUI.indentLevel * 10);
+            if (GUILayout.Button("Attack")) instance.Attack();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(EditorGUI.indentLevel * 10);
+            if (GUILayout.Button("Drop Weapon")) instance.DropWeapon();
+            GUILayout.EndHorizontal();
+
+            EditorGUI.indentLevel--;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
